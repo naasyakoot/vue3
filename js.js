f1c218375f4dd21a0117ac57e105d827078fc771
naasyakoot <naasyakoot@outlook.com>
2020-11-04 22:11:56 +0600

    intag o outtag v-bind not works

--- js.js
+++ js.js
@@ -1,6 +1,18 @@
 var test1id = document.getElementById("test1");
 test1id.innerHTML = "Vue.version: " + Vue.version;
 
+var data = function data() {
 
-var test2id = document.getElementById("test2");
-test2id.innerHTML = "Vue.svgNS: " + Vue.svgNS;
+			return {
+
+				name: "SALAMOON ALAIKOOM",
+				name2: "SALAMOON SALAMOON",
+				name3: "h1 text-warning"
+			};
+
+
+		};
+
+var sets = { data };
+
+	Vue.createApp(sets).mount('#test2');
